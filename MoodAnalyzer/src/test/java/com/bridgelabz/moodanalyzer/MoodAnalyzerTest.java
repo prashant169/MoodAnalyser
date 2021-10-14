fb489d0898ff54db60a4aa0204c8c5af916f6761
prashant169 <prashantphad2018@gmail.com>
2021-10-14 00:14:10 -0800

    Added: Program For The Repeat Test Case 1.1

---
+++ MoodAnalyzer/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+	@Test
+	public void givenMessage_SadMood_Should_Return_Sad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+		String mood = moodAnalyzer.analyseMood();
+		// Asserting The Result As Boolean Type True Or False
+		Assert.assertEquals("SAD", mood);
+
+	}
+}

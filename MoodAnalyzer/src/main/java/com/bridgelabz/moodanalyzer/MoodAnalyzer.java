c5b015e7f7d7a4a10884679ae24f3545d71bbc02
prashant169 <prashantphad2018@gmail.com>
2021-10-14 01:06:18 -0800

    Handle Exception Error If User Provide Null

--- MoodAnalyzer/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,13 +1,24 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
+	private String message;
+
 	public MoodAnalyzer() {
 	}
 
-	public String MoodAnalyzer(String message) {
-		if (message.contains("Sad"))
-			return "SAD";
-		else
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		try {
+
+			if (this.message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
 			return "HAPPY";
+		}
 	}
 }
--- MoodAnalyzer/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,11 +5,17 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 	@Test
-	public void given_AnyMood_Should_Return_HAPPY() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.MoodAnalyzer("I am In Any Mood");
-		Assert.assertEquals("HAPPY", mood);
-
+	public void givenMessage_SadMood_Should_Return_Sad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+		String mood = moodAnalyzer.analyseMood();
+		// Asserting The Result As Boolean Type True Or False
+		Assert.assertEquals("SAD", mood);
 	}
 
+	@Test
+	public void givenMessage_AnyMood_Should_Return_HAPPY() {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
 }
